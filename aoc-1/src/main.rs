3465befba98d3f3bb883e24dec20ceb56728829f
Alex Nikolis <ap.nikolis@gmail.com>
2021-12-01 17:30:28 -0800

    day 1, part 1

---
+++ aoc-1/src/main.rs
@@ -0,0 +1,22 @@
+use std::{
+    fs::File,
+    io::{prelude::*, BufReader}
+};
+
+fn count_depth_increases(depth_readings: &[usize]) -> u32 {
+    depth_readings
+        .windows(2)
+        .fold(0, |acc, w| if w[0] < w[1] { acc + 1} else { acc })
+}
+
+fn main() {
+    let file = File::open("./src/depths.txt").expect("no such file");
+    let buf = BufReader::new(file);
+    let depths: Vec<usize> = buf.lines()
+        .map(|l| l.expect("could not read line").parse::<usize>().unwrap())
+        .collect();
+
+    println!("The number of depth increases is {:?}", count_depth_increases(depths.as_slice()));
+}
+
+
